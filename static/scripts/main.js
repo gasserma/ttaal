6221e876e7fc0634a1376cbce193ee41b20cb910
gasserma <gasserma@gmail.com>
2017-09-30 20:45:03 -0700

    google analytics

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -23,26 +23,25 @@ $(document).ready(function () {
 var right = 0;
 var wrong = 0;
 var firstSelection = true;
+
 function selectionMade(item){
 	$('.result').show(200);
 	$('.source').show(200);
 	
 	if (firstSelection){
 		firstSelection = false;
-		
+	    $('#hoverstyle').remove();
 		var currentCorrect = false;
-		//$('.list-group-item').addClass('disabled');		
 		$('.active').removeClass('active');
 		$('.result').each( function () {
 			if ($(this).data('answer')){
 				$(this).parent().effect("highlight", { color: 'green'}, 1000);
 				
-				if ($(this).parent().attr('id') == item.attr('id')) {
+				if ($(this).parent().parent().attr('id') == item.attr('id')) {
 					right += 1;
 					currentCorrect = true;
 				} else {
 					wrong += 1;
-					//$(this).parent().effect("highlight", { color: 'red'}, 1000);
 				}
 			}
 		});
@@ -58,8 +57,7 @@ function selectionMade(item){
 
 function nextClicked(){	
 	firstSelection = true;
-	$('.active').removeClass('active');	
-	//$('.list-group-item').removeClass('disabled');
+	$('.active').removeClass('active');
 	$('.result').hide();
 	$('.source').hide();
 	
@@ -74,31 +72,32 @@ function nextClicked(){
 	quotes.quotetriplets.splice(index, 1);
 	
 	$('#q1q').text(quote[0].quote);
-	$('#q1r').text(quote[0].result);
+	$('#q1s').text(quote[0].result);
 	if (quote[0].result != "Trump"){
-		$('#q1r').data('answer', true);
+		$('#q1s').data('answer', true);
 	} else {		
-		$('#q1r').data('answer', false);
+		$('#q1s').data('answer', false);
 	}
-	$('#q1s').text(quote[0].source);
+	$('#q1s').attr("href", quote[2].source);
 	
 	$('#q2q').text(quote[1].quote);
-	$('#q2r').text(quote[1].result);	
+	$('#q2s').text(quote[1].result);
 	if (quote[1].result != "Trump"){
-		$('#q2r').data('answer', true);
+		$('#q2s').data('answer', true);
 	} else {		
-		$('#q2r').data('answer', false);
+		$('#q2s').data('answer', false);
 	}
-	$('#q2s').text(quote[1].source);
+	$('#q2s').attr("href", quote[2].source);
 	
 	$('#q3q').text(quote[2].quote);
-	$('#q3r').text(quote[2].result);
+	$('#q3s').text(quote[2].result);
 		if (quote[2].result != "Trump"){
-		$('#q3r').data('answer', true);
+		$('#q3s').data('answer', true);
 	} else {		
-		$('#q3r').data('answer', false);
+		$('#q3s').data('answer', false);
 	}
-	$('#q3s').text(quote[2].source);
+	$('#q3s').attr("href", quote[2].source);
 	
 	$('.btn').addClass('disabled');
+	$(document.body).append('<style id="hoverstyle">.list-group-item:hover { background: gray; cursor: pointer; }</style>');
 }
